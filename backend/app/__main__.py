ded2351f64bb1314cf6c2705674f4358e0e7a623
Lunarmagpie <bambolambo0@gmail.com>
2023-04-29 14:46:26 -0400

    / now serves index.html

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,5 +1,6 @@
 from quart import Quart
 from app import blueprints
+from app.blueprints.files import serve_file
 
 app = Quart(__name__)
 blueprints.load_blueprints(app)
@@ -7,7 +8,7 @@ blueprints.load_blueprints(app)
 
 @app.route("/")
 async def hello():
-    return "hello"
+    return await serve_file("index.html")
 
 
 app.run(port=3000)

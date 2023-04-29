9c969217c237bafe97fd029401b6041a42b6ea67
Lunarmagpie <bambolambo0@gmail.com>
2023-04-29 14:44:21 -0400

    the python server can now serve files

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,9 +1,13 @@
 from quart import Quart
+from app import blueprints
 
 app = Quart(__name__)
+blueprints.load_blueprints(app)
 
-@app.route('/')
+
+@app.route("/")
 async def hello():
-    return 'hello'
+    return "hello"
+
 
 app.run(port=3000)

16f20db636a213932303513242c2f1d5f9f79979
Arogya <arogyadahal2003@gmail.com>
2023-04-30 00:05:19 -0400

    Random

---
+++ frontend/public/info.md
@@ -0,0 +1,11 @@
+## Assets:
+
+---
+
+An asset is an owned belonging that can provide future economic benefit. These goods are durable and can be translated into a dollar amount (‘liquidated’). Common examples include a house, car, or precious metals.
+
+## Cash-checking
+
+---
+
+Cash-checking services describe when a bank can cash an individual’s check, regardless of whether they have a bank account.
--- frontend/src/Pages/Info/Info.jsx
+++ frontend/src/Pages/Info/Info.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+
 const Info = () => {
-  return (
-    <div>
-      <p> THis is info</p>
-      <p> This is info 2</p>
-    </div>
-  );
+  const [data, setData] = useState("");
+  const getData = async (query) => {
+    const response = await fetch("/static/info.md");
+
+    console.log(response.text());
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return <ReactMarkdown>{"# Your markdown here"}</ReactMarkdown>;
 };
 
 export default Info;
